4a63005d6d152cf707f83a19a7d6f94fc16d38b6
NandoStreet <ferazullopez@gmail.com>
2022-04-20 11:57:44 -0500

    React

---
+++ react-pagination/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Countries from 'countries-api';
+import './App.css';
+import Pagination from './components/Pagination';
+import CountryCard from './components/CountryCard';
+
+class App extends Component {
+  state = { allCountries: [], currentCountries: [], currentPage: null, totalPages: null }
+
+  componentDidMount() {
+    const { data: allCountries = [] } = Countries.findAll();
+    this.setState({ allCountries });
+  }
+
+  onPageChanged = data => {
+    const { allCountries } = this.state;
+    const { currentPage, totalPages, pageLimit } = data;
+    const offset = (currentPage - 1) * pageLimit;
+    const currentCountries = allCountries.slice(offset, offset + pageLimit);
+
+    this.setState({ currentPage, currentCountries, totalPages });
+  }
+}
+
+export default App;
